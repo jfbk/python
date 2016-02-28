478f6bdb3ac7aa720e2c81cd3bc20a815cad181d
jorg <jorg@kelpin.nl>
2016-02-28 13:32:06 +0100

    Poes en Beer toegevoegd

---
+++ groen.py
@@ -0,0 +1,13 @@
+#
+# Dit programma is geschreven door Jorg en Fleur.
+# Zo 28 februari 2016
+#
+groen = input('Poes: Is groen jouw lievelingskleur? ')
+if groen == 'ja':
+    naam = input('Poes: Wat is jouw naam? ')
+    print('Poes: je bent mijn vriend '+naam)
+else:
+    kleur = input('Beer: wat is jou lievelings kleur? ')
+    print('Beer: mijn lievelingskleur is ook '+kleur)
+    naam = input('Beer: wat is jouw naam? ')
+    print('Beer: je bent mijn vriend '+naam)
